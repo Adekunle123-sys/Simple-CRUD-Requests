4945a6108132c3834084a77d25ebc4bd29cd30f7
Adekunle Opeyemi <adekunlesamad04@gmail.com>
2025-08-13 09:45:35 +0100

    updated admin.py

--- core/user/admin.py
+++ core/user/admin.py
@@ -3,4 +3,4 @@ from .models import User
 # Register your models here.
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = [ 'firstname', 'lastname']
+    list_display = [ 'firstname', 'lastname', 'email']
